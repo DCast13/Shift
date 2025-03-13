d1a6cc39d2b9f8f064704a2ff7556e76c1e0aff5
DCast13 <dcasti10@uncc.edu>
2025-03-13 12:58:16 -0400

    got reality shifting to work

--- Assets/ModularFirstPersonController/FirstPersonController/RealitySwitcher.cs
+++ Assets/ModularFirstPersonController/FirstPersonController/RealitySwitcher.cs
@@ -1,51 +1,33 @@
 using UnityEngine;
-using UnityEngine.UI;
-
 
 public class RealitySwitcher : MonoBehaviour
 {
-
     public GameObject reality1;
     public GameObject reality2;
     public Material skybox1;
     public Material skybox2;
     private bool isReality1Active = true;
-    private bool isReality2Active = false;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            Debug.Log("Tab Pressed!");     
+            Debug.Log("Tab Pressed!");
             SwitchReality();
         }
     }
 
     void SwitchReality()
     {
-        isReality1Active = !isReality1Active;
+        isReality1Active = !isReality1Active; // Toggle state
 
         reality1.SetActive(isReality1Active);
-        reality2.SetActive(isReality2Active);
+        reality2.SetActive(!isReality1Active); // Corrected
 
-        //SetLayer(reality1, isReality1Active ? "Reality1" : "Ignore Raycast");
-        //Setlayer(reality2, isReality2Active ? "Ignore Raycast" : "Reality2");
-
+        // Swap skybox
         RenderSettings.skybox = isReality1Active ? skybox1 : skybox2;
-        DynamicGI.UpdateEnvironment();
+        DynamicGI.UpdateEnvironment(); // Refresh lighting
 
-        Debug.Log("Reality switched");
-
-    }
-
-    void Setlayer(GameObject obj, string layerName)
-    {
-        obj.layer = LayerMask.NameToLayer(layerName);
-
-        foreach(Transform child in obj.transform)
-        {
-            child.gameObject.layer = LayerMask.NameToLayer(layerName);
-        }
+        Debug.Log("Reality switched to: " + (isReality1Active ? "Reality 1" : "Reality 2"));
     }
 }
