856ff6927811b60c3f55d801081a80796ea538e0
Bryant Lu <clu12@uncc.edu>
2025-03-27 13:19:18 -0400

    implemented basic AI functionality in enemy behavior script

--- Assets/Enemy Manager/EnemyBehavior.cs
+++ Assets/Enemy Manager/EnemyBehavior.cs
@@ -5,6 +5,27 @@ using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public NavMeshAgent enemy;
+    public Transform player;
+    public LayerMask groundLayer, playerLayer;
+    public float health;
+
+    public Vector3 walkPoint;
+    public bool walkPointSet;
+    public float walkPointRange;
+
+    public float attackCooldown;
+    public bool attacked;
+
+    public float sightRange, attackRange;
+    public bool inSightRange, inAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("FirstPersonController").transform;
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +35,99 @@ public class EnemyBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        inSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        inAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        if (!inSightRange && !inAttackRange)
+        {
+            Patrolling();
+        }
+        if (inSightRange && !inAttackRange)
+        {
+            Chasing();
+        }
+        if (inSightRange && inAttackRange)
+        {
+            Attacking();
+        }
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            enemy.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void Chasing()
+    {
+        enemy.SetDestination(player.position);
+    }
+
+    private void Attacking()
+    {
+        enemy.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!attacked)
+        {
+            attacked = true;
+            Invoke(nameof(ResetAttack), attackCooldown);
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ResetAttack()
+    {
+        attacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), .5f);
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
     }
 }
